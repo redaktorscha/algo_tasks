7ce55757fb0ca43d63dc89d7e04b5b1f98734ecb
Maria Nasonkina <nasonkina@yandex.ru>
2023-08-21 22:10:24 +0300

    add primeNumber

---
+++ primeNumber.js
@@ -0,0 +1,39 @@
+const isPrime = (x) => {
+    for (let i = 2; i < x; i += 1) {
+        if (x % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+// const primeNumber1 = (n) => {
+//     const result = [];
+
+//     for (let i = 2; i <= n; i += 1) {
+//         result.push(i);
+//     }
+
+//     return result.filter(x => isPrime(x));
+// }
+
+
+// console.log(primeNumber1(21));
+
+const primeNumber = (n) => {
+    const result = new Array(n + 1).fill(0).map((x, i) => i).slice(2);
+
+    for (let i = 0; i < result.length; i += 1) {
+        if (result[i] !== null && isPrime(result[i])) {
+            for (let j = i + result[i]; j <= result.length; j += result[i]) {
+                result[j] = null;
+            }
+        }
+    }
+
+    return result.filter(x => x !== null);
+
+}
+
+
