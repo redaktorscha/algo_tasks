ef69654d60cf3167ed812bc5a065c4fde904cd9d
Maria Nasonkina <nasonkina@yandex.ru>
2023-08-21 19:20:58 +0300

    add bubblesort 2

--- bubblesort.js
+++ bubblesort.js
@@ -2,21 +2,31 @@ const swap = (i, j, array) => {
     let temp = array[i];
     array[i] = array[j];
     array[j] = temp;
-  };
+};
   
   const bubbleSort = (arr) => {
-    let needSort = true;
+    // let needSort = true;
   
-    while (needSort) {
-      needSort = false;
+    // while (needSort) {
+    //   needSort = false;
   
-      for (let i = 0; i < arr.length; i += 1) {
-        if (arr[i] > arr[i + 1]) {
-          swap(i, i + 1, arr);
-          needSort = true;
+    //   for (let i = 0; i < arr.length; i += 1) {
+    //     if (arr[i] > arr[i + 1]) {
+    //       swap(i, i + 1, arr);
+    //       needSort = true;
+    //     }
+    //   }
+    // }
+
+    for (let i = 0; i < arr.length; i += 1) {
+        for (let j = 0; j < arr.length - i; j += 1) {
+            if (arr[j] > arr[j + 1]) {
+                swap(j, j + 1, arr)
+            }
         }
-      }
     }
   
     return [...arr];
   }
+
+  console.log(bubbleSort([3, 6, 4, 7, 8, 2, 1]));
